8d3d5fbf1bbc15419c857b5212d747c9200a4881
Nildo Júnior <nildo.jnr@gmail.com>
2017-02-07 17:02:35 -0200

    Start to implement algorithm

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ def get_positions(positions_file):
     return pos
 
 def duplicate_vertices(G):
-    G2 = nx.Graph()
+    G_dup = nx.Graph()
     for e in G.edges(data=True):
-        G2.add_edge(e[0] + "1", e[1] + "2", weight=e[2]["weight"])
-        G2.add_edge(e[0] + "2", e[1] + "1", weight=e[2]["weight"])
-    return G2
+        G_dup.add_edge(e[0] + "1", e[1] + "2", weight=e[2]["weight"])
+        G_dup.add_edge(e[0] + "2", e[1] + "1", weight=e[2]["weight"])
+    return G_dup
 
 def duplicate_positions(pos):
     pos2 = {}
@@ -41,12 +41,56 @@ def duplicate_positions(pos):
         pos2[p + "2"] = (v[0] + dist, v[1] - dist)
     return pos2
 
-def draw_graph(G, pos):
-    nx.draw_networkx(G, pos)
+def draw_graph(G, pos, filename=None, paths=None):
+    nx.draw_networkx_nodes(G, pos, node_size=500, node_color="w")
+    nx.draw_networkx_labels(G, pos)
+    nx.draw_networkx_edges(G, pos, alpha=0.2, width=1)
     labels = nx.get_edge_attributes(G, 'weight')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+    if paths:
+        colors = ["r", "b", "g"]
+        for i, path in enumerate(paths):
+            edges = []
+            for i in range(len(path)-1):
+                edges.append((path[i], path[i+1]))
+            color = colors[i % len(colors)]
+            nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color=color, width=2)
     plt.axis('equal')
-    plt.show()
+    if filename:
+        plt.savefig(filename, format="PNG")
+    else:
+        plt.show()
+    plt.clf()
+
+def convert_to_digraph(G):
+    return G.to_directed()
+
+def modify_graph(G, paths):
+    G2 = G.copy()
+    for path in paths:
+        edges = []
+        new_edges = []
+        for i in range(len(path)-1):
+            attrs = G[path[i]][path[i+1]]
+            attrs['weight'] = -attrs['weight']
+            edges.append((path[i], path[i+1]))
+            new_edges.append((path[i+1], path[i], attrs))
+        G2.remove_edges_from(edges)
+        G2.remove_edges_from(new_edges)
+        G2.add_edges_from(new_edges)
+    return G2
+
+def complement_path(path):
+    c_path = []
+    for vertex in path:
+        c_vertex = vertex[0]
+        if vertex[1] == "1":
+            c_vertex += "2"
+        else:
+            c_vertex += "1"
+        c_path.append(c_vertex)
+    return c_path
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -59,14 +103,47 @@ def main():
     args = parser.parse_args()
 
     G = create_graph(args.input_file)
-    G2 = duplicate_vertices(G)
     if args.positions_file:
         pos = get_positions(args.positions_file)
     else:
         pos = nx.spring_layout(G)
-    pos2 = duplicate_positions(pos)
+    draw_graph(G, pos, "graph01.png")
 
-    draw_graph(G2, pos2)
+    G_dup = duplicate_vertices(G)
+    pos2 = duplicate_positions(pos)
+    draw_graph(G_dup, pos2, "graph02.png")
+
+    G_dir = convert_to_digraph(G_dup)
+    draw_graph(G_dir, pos2, "graph03.png")
+
+    sp1 = nx.dijkstra_path(G_dir, "s1", "t1")
+    sp1_len = nx.dijkstra_path_length(G_dir, "s1", "t1")
+
+    sp2 = nx.dijkstra_path(G_dir, "s1", "t2")
+    sp2_len = nx.dijkstra_path_length(G_dir, "s1", "t2")
+
+    draw_graph(G_dir, pos2, "graph04.png", [sp1, sp2])
+
+    if sp1_len <= sp2_len:
+        sp = sp1
+        dest = "t2"
+    else:
+        sp = sp2
+        dest = "t1"
+    csp = complement_path(sp)
+
+    draw_graph(G_dir, pos2, "graph05.png", [sp, csp])
+
+    G_mod = modify_graph(G_dir, [sp, csp])
+    sp = sp[::-1]
+    csp = csp[::-1]
+
+    draw_graph(G_mod, pos2, "graph06.png", [sp])
+
+    sp2 = nx.dijkstra_path(G_mod, "s2", dest)
+    print sp2
+
+    draw_graph(G_mod, pos2, "graph07.png", [sp2])
 
 
 if __name__ == "__main__":
